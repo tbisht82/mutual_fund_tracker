071e41b8c4fb55612f6945cbfb5a460468beec2e
Tilak Bisht <tbisht82@gmail.com>
2020-04-27 22:14:18 +0530

    changes in settings

--- mutual_fund_project/asgi.py
+++ mutual_fund_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mutual_fund_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mutual_fund_project.settings.prod')
 
 application = get_asgi_application()
--- mutual_fund_project/wsgi.py
+++ mutual_fund_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mutual_fund_project.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mutual_fund_project.settings.prod')
 
 application = get_wsgi_application()
