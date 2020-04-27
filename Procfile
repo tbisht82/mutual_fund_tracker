56cba06690ef8b3271bc53077baab1d45bfc3c45
Tilak Bisht <tbisht82@gmail.com>
2020-04-27 19:51:16 +0530

    Configured celery and redis

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mutual_fund_project.wsgi --log-file -
+web: env > .env; env PYTHONUNBUFFERED=true honcho start -f Procfile.real 2>&1
--- mutual_fund_project/settings.py
+++ mutual_fund_project/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 # MEDIA_URL = '/media/'
 
 # Celery configurations
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://') #, 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://') #, 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
